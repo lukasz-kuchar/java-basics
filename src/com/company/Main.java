a8e36bcd26e1851b683859a396b802bab6a3bebf
Lukasz Kuchar <lukaszkuchar1@gmail.com>
2019-12-15 17:53:59 +0100

    added some lines to my 1st java code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Java Rocks!");
         System.out.println("second line");
+        String imie = "lukasz";
+       imie=  imie.toUpperCase();
+        System.out.printf(imie);
     }
 }
